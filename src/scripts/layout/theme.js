04152508adbf861efb081ea1f259b0db15e96b96
Jon Decher <jondecher@gmail.com>
2019-04-10 23:39:54 -0400

    Setup the footer

--- src/scripts/layout/theme.js
+++ src/scripts/layout/theme.js
@@ -16,9 +16,9 @@ focusHash();
 bindInPageLinks();
 
 // Apply a specific class to the html element for browser support of cookies.
-if (cookiesEnabled()) {
-  document.documentElement.className = document.documentElement.className.replace(
-    'supports-no-cookies',
-    'supports-cookies',
-  );
-}
+// if (cookiesEnabled()) {
+//   document.documentElement.className = document.documentElement.className.replace(
+//     'supports-no-cookies',
+//     'supports-cookies',
+//   );
+// }
