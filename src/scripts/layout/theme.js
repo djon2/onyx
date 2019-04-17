25b04dcda0b61e486a98e08e75ee3e46edd7d88f
Jon Decher <jondecher@gmail.com>
2019-04-16 23:25:10 -0400

    Setup the custom icons

--- src/scripts/layout/theme.js
+++ src/scripts/layout/theme.js
@@ -5,8 +5,8 @@ import 'lazysizes/plugins/bgset/ls.bgset';
 import 'lazysizes';
 import 'lazysizes/plugins/respimg/ls.respimg';
 
-import '../../styles/theme.scss';
 import '../../styles/theme.scss.liquid';
+import '../../styles/theme.scss';
 
 import {focusHash, bindInPageLinks} from '@shopify/theme-a11y';
 import {cookiesEnabled} from '@shopify/theme-cart';
--- src/scripts/sections/header.js
+++ src/scripts/sections/header.js
@@ -1,26 +1,11 @@
-import {
-    register,
-} from '@shopify/theme-sections';
-
 const selectors = {
-    mobileMenuToggle: `.js-mobile-menu-toggle`,
-    header: `[data-section-type='header']`
+  mobileMenuToggle: '.js-mobile-menu-toggle',
+  header: '[data-section-type=\'header\']',
 };
 
-
-
-
-// register('header', {
-//     async onLoad() {
-
-
-
-
-//     },
-// });
-
 document.querySelectorAll(selectors.mobileMenuToggle).forEach((button) => {
-    button.onclick = () => {
-        document.querySelector(selectors.header).classList.toggle('menu-is-open');
-    }
+  button.onclick = (event) => {
+    event.preventDefault();
+    document.querySelector(selectors.header).classList.toggle('menu-is-open');
+  };
 });
